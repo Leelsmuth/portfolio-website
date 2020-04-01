98b9f8558db1cccda8e3d2ec069562c774edba19
Leelsmuth <femianjorin@rocketmail.com>
2020-04-01 20:04:11 +0100

    added image to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from 'react'
+import biologo from '../images/femi.jpeg'
+
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <section id="container-about" className="container-about">
+      <h1>About Me</h1>
+
+      <img src={biologo} width="230" height="280" alt="abtimg" />
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                    </p>
+
+    </section>
   )
 }
 
-export default About;
+export default About
