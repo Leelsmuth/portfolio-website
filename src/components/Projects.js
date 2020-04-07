f39ed6323ce9975561930dc01627def0b19d4160
Leelsmuth <femianjorin@rocketmail.com>
2020-04-07 18:48:04 +0100

    wip

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardMenu, CardText, CardActions, Button, IconButton } from 'react-mdl';
-import githubFinder from '../images/githubfinder.JPG';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
+import githubFinder from '../images/githubfinder.gif';
 import darkMode from '../images/darkmode.JPG';
 import topNine from '../images/mytopnine.JPG';
 import trackDrills from '../images/logo.png';
@@ -20,32 +20,24 @@ class Project extends Component {
           <Card shadow={5} style={{ width: '30%', margin: 'auto' }}>
             <CardTitle style={{ color: 'black', height: '300px', background: `url(${githubFinder}) center / cover` }}>Github Finder</CardTitle>
             <CardText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Mauris sagittis pellentesque lacus eleifend lacinia...
-        </CardText>
+              React Project to search github Users, check their details and their latest projects
+            </CardText>
             <CardActions style={{ textAlign: 'center', padding: '16px', background: 'rgba(0,0,0,0.2)' }}>
               <Button colored href="https://github.com/Leelsmuth/github-finder" target='_blank'>Github</Button>
               <Button colored href="https://github-finder-eta-eosin.now.sh/" target='_blank'>LiveDemo</Button>
             </CardActions>
-            <CardMenu style={{ color: '#fff' }}>
-              <IconButton name="share" />
-            </CardMenu>
           </Card>
 
           {/*Project 2 */}
           <Card shadow={5} style={{ width: '30%', margin: 'auto' }}>
             <CardTitle style={{ color: 'white', height: '300px', background: `url(${darkMode}) center / cover` }}>Dark Mode</CardTitle>
             <CardText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Mauris sagittis pellentesque lacus eleifend lacinia...
-        </CardText>
+              Simple React Project using Context API and custom hooks to manipulate the DOM.
+            </CardText>
             <CardActions style={{ textAlign: 'center', padding: '16px', background: 'rgba(0,0,0,0.2)' }}>
               <Button colored href="https://github.com/Leelsmuth/dark-mode" target='_blank'>Github</Button>
               <Button colored href="https://dark-mode-five-mu.now.sh/" target='_blank'>LiveDemo</Button>
             </CardActions>
-            <CardMenu style={{ color: '#fff' }}>
-              <IconButton name="share" />
-            </CardMenu>
           </Card>
 
         </div>
@@ -58,16 +50,12 @@ class Project extends Component {
           <Card shadow={5} style={{ width: '30%', margin: 'auto' }}>
             <CardTitle style={{ color: '', height: '300px', background: `url(${topNine}) center / cover` }}>My Top Nine</CardTitle>
             <CardText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Mauris sagittis pellentesque lacus eleifend lacinia...
+              Simple Redux project that lets you pick your top nine Favourite things in the World (at the moment).
         </CardText>
             <CardActions style={{ textAlign: 'center', padding: '16px', background: 'rgba(0,0,0,0.2)' }}>
               <Button colored href="https://github.com/buildsprint-topnine/web-application" target='_blank'>Github</Button>
               <Button colored href="https://web-application.my-top-nine.now.sh/" target='_blank'>LiveDemo</Button>
             </CardActions>
-            <CardMenu style={{ color: '#fff' }}>
-              <IconButton name="share" />
-            </CardMenu>
           </Card>
 
         </div>
@@ -80,16 +68,12 @@ class Project extends Component {
           <Card shadow={5} style={{ width: '30%', margin: 'auto' }}>
             <CardTitle style={{ color: 'black', height: '300px', background: `url(${trackDrills}) center / cover` }}>Track Drills</CardTitle>
             <CardText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Mauris sagittis pellentesque lacus eleifend lacinia...
+              A website that inspires you to live a healthier lifestyle and tracks your fitness goals
         </CardText>
             <CardActions style={{ textAlign: 'center', padding: '16px', background: 'rgba(0,0,0,0.2)' }}>
               <Button colored href="https://github.com/LABS-EU3/wt_backend" target='_blank'>Github</Button>
               <Button colored href="https://trackdrills.com/" target='_blank'>LiveDemo</Button>
             </CardActions>
-            <CardMenu style={{ color: '#fff' }}>
-              <IconButton name="share" />
-            </CardMenu>
           </Card>
 
         </div>
